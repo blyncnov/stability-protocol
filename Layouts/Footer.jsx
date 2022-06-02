f98eab3654afa292ec49525c347ccf764003c7d3
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-06-02 22:20:54 +0100

    Stability Protocol vision alignment project footer

--- Layouts/Footer.jsx
+++ Layouts/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
+import { MaxWidthComponent } from "../styles/constants/helpers";
+import { Footer__Container } from "../styles/Homepage/Footer";
+
 const Footer = () => {
   return (
-    <>
-      <h2>Footer</h2>
-    </>
+    <Footer__Container>
+      <MaxWidthComponent>
+        <h6>© A2Z Finance, 2022. | All Rights Reserved</h6>
+      </MaxWidthComponent>
+    </Footer__Container>
   );
 };
 
---
+++ styles/Homepage/Footer.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Footer__Container = styled.div`
+  width: 100%;
+  padding: 2em 0;
+  color: #ffffff;
+  background-color: #00154f;
+  background: rgb(22, 16, 133);
+  background: url(/bg1.png),
+    linear-gradient(
+      70deg,
+      rgba(22, 16, 133, 1) 0%,
+      rgba(20, 38, 85, 1) 35%,
+      rgba(19, 130, 153, 1) 100%
+    ),
+    url(/bg4.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: botom right;
+  margin: 2em 0 0 0;
+  text-align: center;
+
+  &::after {
+    content: "fff";
+    position: absolute;
+    top: -10px;
+    width: 50vw;
+    height: 10px;
+  }
+`;
+
+export default Footer__Container;
