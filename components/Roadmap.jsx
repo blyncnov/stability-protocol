8674c7421f916b8db8e49005cad376aabb4a1e9d
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-06-09 19:08:05 +0100

    stability protocol roadmaps v2

--- components/Roadmap.jsx
+++ components/Roadmap.jsx
@@ -6,21 +6,24 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import { MaxWidthComponent } from "../styles/constants/helpers";
 import { FaBriefcase } from "react-icons/fa";
- 
+
 const Roadmap = () => {
   return (
     <MaxWidthComponent>
+      <div style={{ color: "#333" }} className="RoadMap__header">
+        <h1>Our Roadmap</h1>
+      </div>
       <VerticalTimeline>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{
-            background: "rgb(33, 150, 243)",
-            color: "#fff",
+            background: "rgb(22, 16, 133)",
+            color: "#333",
             fontFamily: "'Hind Guntur', sans-serif",
           }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(22, 16, 133)" }}
           date="2011 - present"
-          iconStyle={{ background: "blue", color: "#fff" }}
+          iconStyle={{ background: "#923917", color: "#fff" }}
           icon={<FaBriefcase />}
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
@@ -33,13 +36,13 @@ const Roadmap = () => {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{
-            background: "rgb(33, 150, 243)",
-            color: "#fff",
+            background: "rgb(22, 16, 133)",
+            color: "#333",
             fontFamily: "'Hind Guntur', sans-serif",
           }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(22, 16, 133)" }}
           date="2011 - present"
-          iconStyle={{ background: "blue", color: "#fff" }}
+          iconStyle={{ background: "#923917", color: "#fff" }}
           icon={<FaBriefcase />}
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
@@ -52,13 +55,13 @@ const Roadmap = () => {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{
-            background: "rgb(33, 150, 243)",
-            color: "#fff",
+            background: "rgb(22, 16, 133)",
+            color: "#333",
             fontFamily: "'Hind Guntur', sans-serif",
           }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(22, 16, 133)" }}
           date="2011 - present"
-          iconStyle={{ background: "blue", color: "#fff" }}
+          iconStyle={{ background: "#923917", color: "#fff" }}
           icon={<FaBriefcase />}
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
@@ -71,13 +74,32 @@ const Roadmap = () => {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{
-            background: "rgb(33, 150, 243)",
-            color: "#fff",
+            background: "rgb(22, 16, 133)",
+            color: "#333",
             fontFamily: "'Hind Guntur', sans-serif",
           }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(22, 16, 133)" }}
           date="2011 - present"
-          iconStyle={{ background: "blue", color: "#fff" }}
+          iconStyle={{ background: "#923917", color: "#fff" }}
+          icon={<FaBriefcase />}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project
+            Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work bg-neu"
+          contentStyle={{
+            background: "rgb(22, 16, 133)",
+            color: "#333",
+            fontFamily: "'Hind Guntur', sans-serif",
+          }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(22, 16, 133)" }}
+          date="2011 - present"
+          iconStyle={{ background: "#923917", color: "#fff" }}
           icon={<FaBriefcase />}
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
@@ -90,13 +112,13 @@ const Roadmap = () => {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{
-            background: "rgb(33, 150, 243)",
-            color: "#fff",
+            background: "rgb(22, 16, 133)",
+            color: "#333",
             fontFamily: "'Hind Guntur', sans-serif",
           }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(22, 16, 133)" }}
           date="2011 - present"
-          iconStyle={{ background: "blue", color: "#fff" }}
+          iconStyle={{ background: "#923917", color: "#fff" }}
           icon={<FaBriefcase />}
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
@@ -109,32 +131,13 @@ const Roadmap = () => {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{
-            background: "rgb(33, 150, 243)",
-            color: "#fff",
+            background: "rgb(22, 16, 133)",
+            color: "#333",
             fontFamily: "'Hind Guntur', sans-serif",
           }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(22, 16, 133)" }}
           date="2011 - present"
-          iconStyle={{ background: "blue", color: "#fff" }}
-          icon={<FaBriefcase />}
-        >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{
-            background: "rgb(33, 150, 243)",
-            color: "#fff",
-            fontFamily: "'Hind Guntur', sans-serif",
-          }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          date="2011 - present"
-          iconStyle={{ background: "blue", color: "#fff" }}
+          iconStyle={{ background: "#923917", color: "#fff" }}
           icon={<FaBriefcase />}
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
